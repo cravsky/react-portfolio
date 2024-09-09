ed135dd3c9b3f7e4fc79e4564d099d6002d10ad0
cravsky <cravsky@gmail.com>
2024-09-09 10:52:43 +0200

    Added hero image, corrected margins

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,20 +8,13 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
+        <p className={styles.description}>Frontend Developer</p>
         <h1 className={styles.title}>Hi, I'm Krzysztof</h1>
-        <h2 className={styles.description}>
-        Frontend Developer
-        </h2>
-        <p className={styles.description}>
-        Legacy Code? No Problem.
-        </p>
+        <p className={styles.description}>Legacy Code? No Problem.</p>
         <Mailto className={styles.contactBtn} email="cravsky@gmail.com" obfuscated={true} >Contact Me</Mailto>
-        {/* <a href="mailto:myemail@email.com" className={styles.contactBtn}>
-          Contact Me
-        </a> */}
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/heroImage3.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
